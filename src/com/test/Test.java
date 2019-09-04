bc106ee7f6e0e6fa082d26da6ce0f675fa9eeef8
wangpeng <wan856eng@163.com>
2019-09-04 14:41:22 +0800

    本地到github仓

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,5 +3,9 @@ package com.test;
 public class Test {
     public static void main(String args[]){
         System.out.println("Hello World!");
+        for (int i=0;i<10;i++){
+            i++;
+            System.out.println(i);
+        }
     }
 }
